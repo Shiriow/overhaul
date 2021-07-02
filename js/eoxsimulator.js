f9783444fdf0661b42ebcfebf5c43c6cd9a664fe
Alexow <alexowneves@gmail.com>
2021-07-02 01:53:13 +0100

    default

--- js/eoxsimulator.js
+++ js/eoxsimulator.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 class EOXSimulator extends Simulator {
   get defaultClass() {
-    return "Nightseeker";
+    return "Shogun";
   }
 
   get levelCaps() {
